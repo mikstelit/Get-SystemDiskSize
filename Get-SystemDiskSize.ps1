7c9478a118df2899e87cc51a9a1be9d747aadfa5
Mike.Littlefield@live.com <mike.littlefield@live.com>
2016-09-26 12:12:43 -0700

    Initial commit

---
+++ Get-SystemDiskSize.ps1
@@ -0,0 +1,55 @@
+﻿<#
+.Synopsis
+   Gets the size and free space of a drive from the list of computers. 
+.DESCRIPTION
+   The script takes either one or more computer names and outputs the 
+   computer's name, disk size, and free space.  Only one disk is investigated 
+   and by default that is the C drive, but this can be changed by specifying 
+   the Drive parameter in the format of D:.
+.EXAMPLE
+   Get-SystemDiskSize $ComputerNames
+.EXAMPLE
+   $ComputerNames | Get-SystemDiskSize
+#>
+function Get-SystemDiskSize
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$True,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$True,
+                   Position=0)]
+        [string[]]$ComputerNames,
+
+        [string]$LogName = ".\SystemDisks.csv",
+
+        [string]$Drive = "C:"
+    )
+
+    Process
+    {
+        Foreach ($ComputerName in $ComputerNames)
+        {
+            Try
+            {
+                $Disk = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $ComputerName | Where-Object { $_.DeviceID -eq $Drive } -ErrorAction Stop
+                [psobject]$System = @{
+                    'Name' = $ComputerName;
+                    'DiskSize' = $Disk.Size / 1GB;
+                    'FreeSpace' = $Disk.FreeSpace / 1GB;
+                }
+            }
+            Catch
+            {
+                [psobject]$System = @{
+                    'Name' = $ComputerName;
+                    'DiskSize' = $Error[0].Exception.Message;
+                    'FreeSpace' = '';
+                }
+            }
+
+            Write-Output  ('{0},{1:N1},{2:N1}' -f $System.Name,$System.DiskSize,$System.FreeSpace)
+        }
+    }
+}
